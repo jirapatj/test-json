6e08d2ea014e28277bb37f2ea2293a50c542eecf
jiraj <jewel.jirapat@gmail.com>
2024-05-16 02:19:58 +0700

    add

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.process();
+    }
+
+    private void process() {
+        System.out.println("Hello world!");
+    }
+}
