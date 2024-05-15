6257d9a1dfeb7a29a565b3adbeec9d917029a080
jiraj <jewel.jirapat@gmail.com>
2024-05-16 02:53:02 +0700

    + add

--- src/main/java/org/example/FindLevel.java
+++ src/main/java/org/example/FindLevel.java
@@ -2,38 +2,33 @@ package org.example;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Map;
 
-public class Main {
-    public static void main(String[] args) {
-        try {
-            Main m = new Main();
-            m.process();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+public class FindLevel {
+    public static void main(String[] args) throws IOException {
+        String jsonString = new String(FindLevel.class.getResourceAsStream("/input.json").readAllBytes());
+        // Read JSON string
 
-    private void process() throws IOException {
-        // Read json file
-        String jsonString = new String(getClass().getResourceAsStream("/input.json").readAllBytes());
+        // Create ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
 
         // Read JSON string
-        JsonNode jsonNode = new ObjectMapper().readTree(jsonString);
+        JsonNode jsonNode = objectMapper.readTree(jsonString);
+
+        // Specify the level to search for
         int targetLevel = 2;
-        processJsonAtLevel(jsonNode, targetLevel, 0, "");
+
+        // Find all attributes at the specified level
+        findAttributesAtLevel(jsonNode, targetLevel, 1, "");
     }
 
-    private void processJsonAtLevel(JsonNode jsonNode, int targetLevel, int currentLevel, String parentName) {
+    private static void findAttributesAtLevel(JsonNode jsonNode, int targetLevel, int currentLevel, String parentName) {
         if (currentLevel == targetLevel) {
             // If the current level matches the target level, print attributes with parent name
-            printAttributes(jsonNode, parentName);
+            printAttributes(jsonNode, parentName, currentLevel);
         } else if (currentLevel < targetLevel) {
             // If current level is less than the target level, continue recursion
             if (jsonNode.isObject()) {
@@ -42,23 +37,23 @@ public class Main {
                     Map.Entry<String, JsonNode> field = fieldsIterator.next();
                     String attributeName = field.getKey();
                     JsonNode fieldValue = field.getValue();
-                    processJsonAtLevel(fieldValue, targetLevel, currentLevel + 1, attributeName);
+                    findAttributesAtLevel(fieldValue, targetLevel, currentLevel + 1, attributeName);
                 }
             } else if (jsonNode.isArray()) {
                 for (JsonNode node : jsonNode) {
-                    processJsonAtLevel(node, targetLevel, currentLevel + 1, parentName);
+                    findAttributesAtLevel(node, targetLevel, currentLevel + 1, parentName);
                 }
             }
         }
     }
 
-    private void printAttributes(JsonNode jsonNode, String parentName) {
+    private static void printAttributes(JsonNode jsonNode, String parentName, int level) {
         if (jsonNode.isObject()) {
             Iterator<Map.Entry<String, JsonNode>> fieldsIterator = jsonNode.fields();
             while (fieldsIterator.hasNext()) {
                 Map.Entry<String, JsonNode> field = fieldsIterator.next();
                 String attributeName = field.getKey();
-                System.out.println("Parent: " + parentName + ", Attribute: " + attributeName);
+                System.out.println("Parent: " + parentName + ", Level: " + level + ", Attribute: " + attributeName);
             }
         }
     }
